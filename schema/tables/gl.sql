784c7533bb096c21e03bcd0159a7bd6f4d8aa5ff
Paul Trowbridge <fleetside72@hotmail.com>
2018-09-17 23:42:01 -0400

    gl columns

--- schema/tables/gl.sql
+++ schema/tables/gl.sql
@@ -2,13 +2,15 @@
 
 CREATE TABLE evt.gl (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
-    ,bpr_id INT REFERENCES evt.bpr (id)
+    ,bprid INT REFERENCES evt.bpr (id)
     ,acct text REFERENCES evt.acct (acct)
     ,amount numeric (12,2)
-    ,gl_line INT
-    ,bpr_extract JSONB
+    ,glline INT
+    ,bprkeys JSONB
 );
-COMMENT ON COLUMN evt.gl.bpr_id IS 'id of initial basic pecuniary record';
+COMMENT ON COLUMN evt.gl.id IS 'gl id';
+COMMENT ON COLUMN evt.gl.bprid IS 'id of initial basic pecuniary record';
 COMMENT ON COLUMN evt.gl.acct IS 'account code';
 COMMENT ON COLUMN evt.gl.amount IS 'amount';
-COMMENT ON COLUMN evt.gl.bpr IS 'extract from initial basic pecuniary record';
+COMMENT ON COLUMN evt.gl.glline IS 'gl line number';
+COMMENT ON COLUMN evt.gl.bprkeys IS 'extract from initial basic pecuniary record';
