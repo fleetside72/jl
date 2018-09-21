ab96a07e70f0861f927b8b67d1520ce651c8bfb2
Paul Trowbridge <fleetside72@hotmail.com>
2018-09-20 20:04:08 -0400

    merge period approaches

--- schema/suggested_data/periods.sql
+++ schema/suggested_data/periods.sql
@@ -30,3 +30,24 @@ FROM
         m.calendar_month = extract(month from gs.d)
 ORDER BY 
     gs.d ASC
+/*
+INSERT INTO
+    evt.fspr
+SELECT 
+    --TO_CHAR(gs.d,'YYYY.MM.DD')::ltree t1
+    (
+        --year
+        to_char(extract(year from gs.d),'FM0000')
+        --quarter
+        ||'.'||to_char(m.fq,'FM00')
+        --month
+        ||'.'||to_char(m.fm,'FM00')
+        --day
+        ||'.'||to_char(extract(day from gs.d),'FM00')
+    )::ltree t2
+    ,tstzrange(gs.d,gs.d + '1 month'::interval) r
+FROM 
+    generate_series('2018-01-01 00:00'::timestamptz,'2099-12-01 00:00'::timestamptz,'1 day') gs(d)
+    INNER JOIN m ON
+        m.cm = extract(month from gs.d)
+*/
