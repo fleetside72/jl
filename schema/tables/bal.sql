e3c0530787b612ed0aebee3355b3f347ffcda495
Paul Trowbridge <fleetside72@hotmail.com>
2018-09-20 23:58:34 -0400

    add primary key and chane column name

--- schema/tables/bal.sql
+++ schema/tables/bal.sql
@@ -2,14 +2,15 @@
 
 CREATE TABLE evt.bal (
     acct ltree REFERENCES evt.acct(acct)
-    ,perd ltree REFERENCES evt.fspr(id)
+    ,fspr ltree REFERENCES evt.fspr(id)
     ,obal numeric(12,2)
     ,debits numeric(12,2)
     ,credits numeric(12,2)
     ,cbal numeric(12,2)
 );
+ALTER TABLE evt.bal ADD CONSTRAINT bal_pk PRIMARY KEY(acct,fspr);
 COMMENT ON COLUMN evt.bal.acct IS 'account';
-COMMENT ON COLUMN evt.bal.perd IS 'period';
+COMMENT ON COLUMN evt.bal.fspr IS 'period';
 COMMENT ON COLUMN evt.bal.obal IS 'opening balance';
 COMMENT ON COLUMN evt.bal.debits IS 'total debits';
 COMMENT ON COLUMN evt.bal.credits IS 'total credits';
