3321c7c614c955a96ecca99012849eca6f572299
Paul Trowbridge <ptrowbridge@hc-comapnies.com>
2018-09-28 16:28:06 -0400

    add retained earnings constraint per trial balance

--- db_deploy.sql
+++ db_deploy.sql
@@ -31,6 +31,9 @@ COMMENT ON TABLE evt.acct IS 'account master list';
 COMMENT ON COLUMN evt.acct.acct IS 'account';
 COMMENT ON COLUMN evt.acct.prop IS 'properties';
 
+--this should effectively only allow one instance of an account where retained_earnings = set per top level account (trial balance)
+CREATE UNIQUE INDEX acct_re ON evt.acct (subpath(acct,0,1)) WHERE prop ->> 'retained_earnings' = 'set';
+
 ------------------------fiscal periods------------------------
 CREATE TABLE evt.fspr (
     id ltree PRIMARY KEY
