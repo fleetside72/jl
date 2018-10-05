bc331be07c6bc9083fa59a347971b4ebc4b9d353
Paul Trowbridge <ptrowbridge@hc-comapnies.com>
2018-10-05 14:31:36 -0400

    add re constraint from yeclose

--- schema/tables/acct.sql
+++ schema/tables/acct.sql
@@ -8,3 +8,6 @@ CREATE TABLE evt.acct (
 COMMENT ON TABLE evt.acct IS 'account master list';
 COMMENT ON COLUMN evt.acct.acct IS 'account';
 COMMENT ON COLUMN evt.acct.prop IS 'properties';
+
+--this should effectively only allow one instance of an account where retained_earnings = set per top level account (trial balance)
+CREATE UNIQUE INDEX acct_re ON evt.acct (subpath(acct,0,1)) WHERE prop ->> 'retained_earnings' = 'set';
