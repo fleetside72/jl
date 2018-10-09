43720c3716bcda892a4aa02f81c5347059ba41e3
Paul Trowbridge <ptrowbridge@hc-comapnies.com>
2018-10-09 16:51:21 -0400

    replace account in path

---
+++ schema/functions/reset_account.sql
@@ -0,0 +1,15 @@
+select
+        g.id
+        ,jp.j::text[]
+        ,jp.rn
+        ,jp.j::text[]
+        ,jsonb_pretty(jsonb_set(bpr,(jp.j::text[]||ARRAY['account']),'"h.cars"'))
+        ,jsonb_pretty(bpr)
+from
+        evt.gl g
+        inner join evt.bpr b on
+                b.id = g.bprid
+        join lateral jsonb_array_elements_text(b.bpr->'gl'->'jpath'->glline) WITH ORDINALITY jp(j, rn) on
+                b.bpr#>(jp.j::text[])->>'account' = g.acct::text
+where
+        g.acct = 'h.maint.cars';
