c9c0a48d79f2ab80228b9110f98cd4503414f139
Paul Trowbridge <ptrowbridge@hc-comapnies.com>
2018-10-09 17:18:42 -0400

    create function based on regexp replace

--- schema/functions/reset_account.sql
+++ schema/functions/reset_account.sql
@@ -17,3 +17,41 @@ where
 
 
 select regexp_replace(bpr::text,'"account": "h.maint.cars"','"account": "h.cars"','g') from evt.bpr where bpr::text ~ '"account": "h.maint.cars"'
+
+DO
+$f$
+DECLARE
+    _json jsonb;
+    _key text;
+    _oldval text;
+    _newval text;
+    _old text;
+    _new text;
+BEGIN
+    _json := '{"gl": {"jpath": [["{item,0}", "{header}"], ["{item,0}", "{header}"]], "lines": [{"amount": 4.00, "account": "h.cars"}, {"amount": -4.00, "account": "h.dcard"}]}, "item": [{"item": "light bulb", "amount": 4.00, "reason": "repair", "account": "h.cars"}], "header": {"date": "2018-09-01", "entity": "home", "module": "MHI", "offset": "h.dcard", "transaction": "purchase"}}'::jsonb;
+    _key := 'account';
+    _oldval := 'h.cars';
+    _newval := 'h.x';
+    _old := '"'||_key||'": "'||_oldval||'"';
+    _new := '"'||_key||'": "'||_newval||'"';
+    RAISE NOTICE '%',_old;
+    RAISE NOTICE '%',_new;
+    RAISE NOTICE '%',regexp_replace(_json::text,'"'||_key||'": "'||_oldval||'"','"'||_key||'": "'||_newval||'"','g');
+END;
+$f$
+language plpgsql
+
+DROP FUNCTION IF EXISTS evt.kv_replace(_jsonb jsonb, _key text, _oldval text, _newval text);
+CREATE OR REPLACE FUNCTION evt.kv_replace(_jsonb jsonb, _key text, _oldval text, _newval text) RETURNS jsonb
+AS
+$f$
+DECLARE
+    _result jsonb;
+BEGIN
+
+    SELECT regexp_replace(_jsonb::text,'"'||_key||'": "'||_oldval||'"','"'||_key||'": "'||_newval||'"','g') INTO _result;
+
+    RETURN _result;
+END;
+$f$
+language plpgsql
